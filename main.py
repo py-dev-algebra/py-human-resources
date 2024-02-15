fef5b92e9d767b207b6b7df55b155a2dd17c5980
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-02-15 20:29:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from services.db_services.db_mgr import create_table
 from commons.sql_queries import CREATE_TABLE_EMPLOYEES_QUERY
 
 
-
 def init_db():
     message = create_table(CREATE_TABLE_EMPLOYEES_QUERY)
     print(message)
