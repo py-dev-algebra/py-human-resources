dc272ddd53f9da9ae64ac06b1eaad26365efe3a8
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-02-15 19:06:32 +0100

    Update employee_webapi_mgr.py

--- services/employee_services/employee_webapi_mgr.py
+++ services/employee_services/employee_webapi_mgr.py
@@ -2,3 +2,8 @@
 # na REST api serveru
 import requests
 
+
+# TODO implement try except
+response = requests.get('https://jsonplaceholder.typicode.com/users/1')
+if response.status_code == 200:
+    print(response.json())
